e9e88771e6dc8a79c223d1bcf86d65032a6c041e
ScottMorris <404967+ScottMorris@users.noreply.github.com>
2026-02-27 07:13:00 +0000

    remove vite bug workaround from createComponent
    The workaround using `createElement` instead of `React.createElement` was introduced to bypass Vite bug #6104. This bug has been resolved in newer versions of Vite. This change reverts to using `React.createElement` directly, removing the unnecessary import and comment.
    
    Ref: https://github.com/vitejs/vite/issues/6104

--- packages/ontario-design-system-component-library-react/src/react-component-lib/createComponent.tsx
+++ packages/ontario-design-system-component-library-react/src/react-component-lib/createComponent.tsx
@@ -1,4 +1,4 @@
-import React, { createElement } from 'react';
+import React from 'react';
 
 import { attachProps, camelToDashCase, createForwardRef, dashToPascalCase, isCoveredByReact, mergeRefs } from './utils/index.js';
 
@@ -82,14 +82,7 @@ export const createReactComponent = <
         style,
       };
 
-      /**
-       * We use createElement here instead of
-       * React.createElement to work around a
-       * bug in Vite (https://github.com/vitejs/vite/issues/6104).
-       * React.createElement causes all elements to be rendered
-       * as <tagname> instead of the actual Web Component.
-       */
-      return createElement(tagName, newProps, children);
+      return React.createElement(tagName, newProps, children);
     }
 
     static get displayName() {
